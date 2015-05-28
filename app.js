3d4bfa5d03b0af9908e51187a05be55ab59da759
comasd <comas_d@uoc.edu>
2015-05-28 01:35:39 +0100

    Canvi de port web de 5000 a 80

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app     = express();
 
 // set the port based on environment (more on environments later)
-var port    = 5000;
+var port    = 80;
 
 // send our index.html file to the user for the home page
 app.get('/', function(req, res) {
