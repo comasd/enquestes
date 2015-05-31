2604ce527ac068487408912bca9f227b4c55e92c
comasd <comas_d@uoc.edu>
2015-05-31 21:28:50 +0100

    Codi correcte amb 2 arxius spec que passen tots els test(8)

--- test/spec_veure_crear_enquestes.js
+++ test/spec_veure_crear_enquestes.js
@@ -5,10 +5,20 @@ describe('Comprovar acces a les enquestes ', function() {
     var usuariNom = element(by.model('login.loginData.username'));
     var usuariPassword = element(by.model('login.loginData.password'));
     var botoConfirmaLogin = element(by.id('botoConfirmaLogin'));
+    var botoLogin = element(by.id('botoLogin'));
 
     it('should be able to login as UOC user and see polls', function() {
         browser.get('http://localhost:8000');
-        botoVeureEnquestes.click();
+        botoLogin.isPresent().then(function(result){
+            if (result) {
+                botoLogin.click();
+                usuariNom.sendKeys('UOC');
+                usuariPassword.sendKeys('test');
+                botoConfirmaLogin.click();
+            } else {
+                botoVeureEnquestes.click();
+            }
+        });
         expect(browser.getCurrentUrl()).toBe('http://localhost:8000/polls');
       });
 
