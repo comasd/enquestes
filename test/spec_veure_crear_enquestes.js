561356a48938d516295c273418814dbe9b4724f9
comasd <comas_d@uoc.edu>
2015-05-31 20:33:13 +0100

    Afegint els test anteriors, arxiu spec.js i el nou spec_veure ... amb correcio veure enquestes boto

--- test/spec_veure_crear_enquestes.js
+++ test/spec_veure_crear_enquestes.js
@@ -1,17 +1,14 @@
 // spec veure i crear enquestes
 describe('Comprovar acces a les enquestes ', function() {
     var message = element(by.id('benvingudaMessage'));
-    var botoLogin = element(by.id('botoLogin'));
+    var botoVeureEnquestes = element(by.id('botoLoginVeureEnquestes'));
     var usuariNom = element(by.model('login.loginData.username'));
     var usuariPassword = element(by.model('login.loginData.password'));
     var botoConfirmaLogin = element(by.id('botoConfirmaLogin'));
 
     it('should be able to login as UOC user and see polls', function() {
         browser.get('http://localhost:8000');
-        botoLogin.click();
-        usuariNom.sendKeys('UOC');
-        usuariPassword.sendKeys('test');
-        botoConfirmaLogin.click();
+        botoVeureEnquestes.click();
         expect(browser.getCurrentUrl()).toBe('http://localhost:8000/polls');
       });
 
